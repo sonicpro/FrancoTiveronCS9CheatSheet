0b9e4439a6be77949388c778cab841947d17e705
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-02-28 17:29:45 +0200

    Renamed the solution to FrancoTiveronCS9CheetSheet. Added default interface members demo.

---
+++ FrancoTiveronCS9CheatSheet/CS8/DefaultInterfaceMembers/Application.cs
@@ -0,0 +1,20 @@
+﻿namespace FrancoTiveronCS9CheatSheet.CS8.DefaultInterfaceMembers
+{
+    public class ApplicationV1 : OldWayOfInterfaceAugmenting.ILibrary
+    {
+        public int Increment(int i) => ++i;
+    }
+
+    public class ApplicationV2 : OldWayOfInterfaceAugmenting.ILibrary
+    {
+        public int Increment(int i) => ++i;
+
+        public int Decrement(int i) => --i;
+    }
+
+    // This class is the same as the ApplicatoinV1. However it implements CS8WayOfInterfaceAurmenting.ILibrary interface, which has 2 members.
+    public class Application: CS8WayOfInterfaceAugmenting.ILibrary
+    {
+        public int Increment(int i) => ++i;
+    }
+}
---
+++ FrancoTiveronCS9CheatSheet/CS8/DefaultInterfaceMembers/ILibrary.cs
@@ -0,0 +1,30 @@
+﻿namespace FrancoTiveronCS9CheatSheet.CS8.DefaultInterfaceMembers
+{
+    namespace OldWayOfInterfaceAugmenting
+    {
+        public interface ILibrary
+        {
+            int Increment(int i);
+        }
+
+        // Old way of the interface augmenting - imposes a new interface.
+        public interface ILibraryV2 : ILibrary
+        {
+            int Decrement(int i);
+        }
+    }
+
+
+    namespace CS8WayOfInterfaceAugmenting
+    {
+        // Declaring the new member as having the default implementation does not break the interface compatibility.
+        // Users that only implement the Increment member do not have to rewrite their code to conform to the updated interface.
+        public interface ILibrary
+        {
+            int Increment(int i);
+
+            // Users that 
+            int Decrement(int i) => --i;
+        }
+    }
+}
