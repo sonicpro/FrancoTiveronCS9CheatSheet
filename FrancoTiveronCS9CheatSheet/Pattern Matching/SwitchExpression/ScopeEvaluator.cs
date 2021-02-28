c1b8ac3f9c7754d3b3d57ec5872c24e7f1fd6bec
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-02-28 20:15:54 +0200

    Added Switch Expression demo.

---
+++ FrancoTiveronCS9CheatSheet/Pattern Matching/SwitchExpression/ScopeEvaluator.cs
@@ -0,0 +1,53 @@
+﻿namespace FrancoTiveronCS9CheatSheet.Pattern_Matching.SwitchExpression
+{
+    public static class ScopeEvaluator
+    {
+        public static string SwitchStatement(int score)
+        {
+            string resultDescription = string.Empty;
+
+            switch (score)
+            {
+                case 0:
+                    resultDescription = "Unclassified";
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    resultDescription = "Bad";
+                    break;
+                case 4:
+                case 5:
+                    resultDescription = "Ordinary";
+                    break;
+                case 6:
+                case 7:
+                    resultDescription = "Good";
+                    break;
+                case 8:
+                case 9:
+                    resultDescription = "Excellent";
+                    break;
+                case 10:
+                    resultDescription = "Outstanding";
+                    break;
+                default:
+                    resultDescription = "Invalid Score";
+                    break;
+            }
+
+            return resultDescription;
+        }
+
+        public static string SwitchExpression(int score) => score switch
+        {
+            0 => "Unclassified",
+            >0 and <= 3 => "Bad",
+            4 or 5 => "Ordinary",
+            6 or 7 => "Good",
+            8 or 9 => "Excellent",
+            10 => "Outstanding",
+            _ => "Invalid Score"
+        };
+    }
+}
---
+++ XUnitTests/SwitchExpression.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Xunit;
+using static FrancoTiveronCS9CheatSheet.Pattern_Matching.SwitchExpression.ScopeEvaluator;
+
+namespace XUnitTests
+{
+    public class SwitchExpression
+    {
+        [Fact]
+        public void Test()
+        {
+            // Enumarate over "scores" from -1 to 10 and test whether switch expression
+            // matches the scores correctly.
+            Assert.All(
+                Enumerable.Range(-1, 12),
+                s => Assert.Equal(SwitchStatement(s), SwitchExpression(s)));
+        }
+    }
+}
