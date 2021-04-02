13a074fefa19b34d0a74fa8a9bac8b36e0b62f0a
Volodymyr Tsurko <v-votsur@microsoft.com>
2021-04-02 16:28:00 +0300

    Added record type for TargetTypeInference demonstration.

---
+++ FrancoTiveronCS9CheatSheet/TargetTypeInference/MyType.cs
@@ -0,0 +1,8 @@
+﻿namespace FrancoTiveronCS9CheatSheet.TargetTypeInference
+{
+	// Short-hand record declaration.
+	// This declares "MyType" record with "Value" public init-only autoproperty and the paremeterized constructor with the default parameter of 0.
+	// For the records with 2+ properties Deconstruct method is also created by the compiler.
+	public record MyDataType(int Value = 0);
+	
+}
