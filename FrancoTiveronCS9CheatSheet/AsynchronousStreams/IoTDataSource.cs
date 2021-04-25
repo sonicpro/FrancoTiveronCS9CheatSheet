e03124c13155a263a314e4dbc72db71e2abba167
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-04-25 13:23:55 +0300

    Added the class that simulates sparsed data source. Fetched (async stream) data is processed through IAsyncEnumerable.

---
+++ FrancoTiveronCS9CheatSheet/AsynchronousStreams/IoTDataSource.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrancoTiveronCS9CheatSheet.AsynchronousStreams
+{
+	// Simulates slow answering data source.
+	public static class IoTDataSource
+	{
+		private static SemaphoreSlim[] semaphores;
+
+		// This is the new IAsyncEnumerable interface type.
+		public static async IAsyncEnumerable<JsonElement> DownloadCountriesStream(string[] iso2Codes)
+		{
+			// Simulare "latency" of the spread sensors data source.
+			StartFeed(iso2Codes.Length);
+			for (int i = 0; i < iso2Codes.Length; i++)
+			{
+				// This is the new CS9 keyword "yield return await".
+				yield return await DownloadCountryAsync(i, iso2Codes[i]);
+			}
+		}
+
+		// This method is suitable only for Json in the style pf api.worlbank.org/v2.
+		// Specifically the returned Json array mustcontain the first element for header.
+		public static async Task<JsonElement.ArrayEnumerator> FetchJsonFromUrl(string url)
+		{
+			var webClient = new WebClient();
+			var response = await webClient.DownloadStringTaskAsync(url);
+			JsonDocument responseJson = JsonDocument.Parse(response);
+			JsonElement.ArrayEnumerator array = responseJson.RootElement.EnumerateArray();
+			// Enumerate to the header.
+			array.MoveNext();
+			// Skip the header to the array of countries.
+			array.MoveNext();
+			return array;
+		}
+
+		private static async Task<JsonElement> DownloadCountryAsync(int semaphoreNumber, string iso2Code)
+		{
+			// Proceed only when the semaphore is not zero.
+			await semaphores[semaphoreNumber].WaitAsync();
+			JsonElement.ArrayEnumerator countryJson = await FetchJsonFromUrl($"http://api.worldbank.org/v2/country/{iso2Code}?format=json");
+			// "country" variable represens a single country Json wrapped into array.
+			JsonElement country = countryJson.Current;
+			if (country.ValueKind != JsonValueKind.Array)
+				throw new System.FormatException("Invalid Json Format");
+			return country[0];
+		}
+
+		// When called, initializes semaphores field with n semaphores each zeroed initially.
+		// Release each semaphore after 1 second.
+		private static void StartFeed(int sensorsCount)
+		{
+			async Task Feed()
+			{
+				for (int i = 0; i < sensorsCount; i++)
+				{
+					await Task.Delay(1000);
+					semaphores[i].Release();
+				}
+			};
+			// Each semapure has initial count of 0 and max allowed threads to pass of 1.
+			semaphores = Enumerable.Range(0, sensorsCount).Select(_ => new SemaphoreSlim(0, 1)).ToArray();
+			Task.Run(Feed);
+		}
+	}
+}
---
+++ XUnitTests/TestAsynchronousStream.cs
@@ -0,0 +1,45 @@
+﻿using FrancoTiveronCS9CheatSheet.AsynchronousStreams;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace XUnitTests
+{
+	public class TestAsynchronousStream
+	{
+		private ITestOutputHelper output;
+
+		public TestAsynchronousStream(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		// There is no assert in this test. Just a demonstration of the new "await foreach" keyword.
+		[Fact]
+		public async Task TestStream()
+		{
+			var tenCodes = await GetTenRandomIso2Codes();
+			var countries = IoTDataSource.DownloadCountriesStream(tenCodes);
+			// "await foreach" is a new keyword thatcan be applied to a variable that implements IAsyncEnumerable<T>.
+			await foreach (var country in countries)
+			{
+				output.WriteLine(country.GetProperty("name").GetString());
+			}
+		}
+
+		private async Task<string[]> GetTenRandomIso2Codes()
+		{
+			// Download a hundred Iso2Codes from worldbank.org.
+			JsonElement.ArrayEnumerator array = await IoTDataSource.FetchJsonFromUrl("http://api.worldbank.org/v2/country?format=json&per_page=100");
+			// Remove array wrapper from the Json.
+			JsonElement.ArrayEnumerator countries = array.Current.EnumerateArray();
+			// Select iso2Codes from array of countries.
+			string[] iso2Codes = countries.Select(c => c.GetProperty("iso2Code").GetString()).ToArray();
+			// Take 10 sequential codes from the hundred of country data.
+			var random = new System.Random();
+			return Enumerable.Range(0, 10).Select(i => random.Next(i * 10, i * 10 + 10)).Select(i => iso2Codes[i]).ToArray();
+		}
+	}
+}
